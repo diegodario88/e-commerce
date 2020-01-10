31f6463d8364feecb55580d7a0a1aeded2a0af37
Diego Dario <diegodario88@gmail.com>
2020-01-10 14:10:57 -0300

    smaller components in homepage, finished styling menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './homepage.styles.scss';
+import DirectoryComponent from '../../components/directory/directory.component';
+
+export default function HomepageComponent() {
+
+    return (
+        <div className='homepage'>
+            <DirectoryComponent />
+        </div >
+
+    )
+}
