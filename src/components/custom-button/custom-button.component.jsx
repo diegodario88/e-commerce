0b1ce0a368611a81efe2f101e502b83441858345
Diego Dario <diegodario88@gmail.com>
2020-01-27 02:29:44 -0300

    added cartUtils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
 
-    <button className=
-        {`${isGoogleSignIn ? 'google-signin' : ''} custom-button`}
-        {...otherProps}>
-    
+    <button
+        className={`${inverted ? 'inverted' : ''}
+        ${isGoogleSignIn ? 'google-signin' : ''} 
+        custom-button`} {...otherProps}
+    >
         {children}
-
     </button >
 
 )
